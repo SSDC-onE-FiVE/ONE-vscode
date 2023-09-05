2ad9da5d36c3038dfa5ac37860a2389a12c97873
Bumsoo Ko <rhqjatn2398@naver.com>
2023-09-05 16:25:50 +0900

    [EdgeTPU] Add EdgeTPUDebianToolchain
    - implemented parsing the cfg file to construct command
    - used ini and fs packages to read cfg file
    
    ONE-vscode-DCO-1.0-Signed-off-by: Bumsoo Ko <rhqjatn2398@naver.com>

---
+++ src/Backend/EdgeTPU/EdgeTPUToolchain.ts
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Command } from "../Command";
+import {
+  DebianToolchain,
+} from "../ToolchainImpl/DebianToolchain";
+
+import * as ini from "ini";
+import * as fs from "fs";
+
+class EdgeTPUDebianToolchain extends DebianToolchain {
+  run(cfg: string): Command {
+
+    let cmd = new Command("edgetpu_compiler");
+
+    // TODO: cfg는 cfg file path임
+    //       cfg 파일을 열어 edgetpu_compiler 명령 옵션 이어 붙이기로 Command를 만들어 반환해야함
+
+    var config = ini.parse(fs.readFileSync(cfg, 'utf-8'));    
+    
+    // TODO: 예외 처리 how?
+    if (config["one-import-edgetpu"] === undefined) {
+        return cmd;
+    }
+
+    let outputPath = config["one-import-edgetpu"]["output_path"];
+    cmd.push("--out_dir");
+    cmd.push(outputPath);
+
+    let help = config["one-import-edgetpu"]["help"];
+    if (help === "True") { 
+        cmd.push("--help");
+    } 
+
+    let intermediateTensors = config["one-import-edgetpu"]["intermediate_tensors"];
+    if (intermediateTensors !== undefined) {
+        cmd.push("--intermediate_tensors");
+        cmd.push(intermediateTensors);
+    }
+
+    let showOperations = config["one-import-edgetpu"]["show_operations"];
+    if (showOperations === "True") {
+        cmd.push("--show_operations");
+    }
+
+    let minRuntimeVersion = config["one-import-edgetpu"]["min_runtime_version"];
+    if (minRuntimeVersion !== undefined) {
+        cmd.push("--min_runtime_version");
+        cmd.push(minRuntimeVersion);
+    }
+
+    let searchDelegate = config["one-import-edgetpu"]["search_delegate"];
+    if (searchDelegate === "True") {
+        cmd.push("--search_delegate");
+    }
+
+    let delegateSearchStep = config["one-import-edgetpu"]["delegate_search_step"];
+    if (delegateSearchStep !== undefined) {
+        cmd.push("--delegate_search_step");
+        cmd.push(delegateSearchStep);
+    }
+
+    let inputPath = config["one-import-edgetpu"]["input_path"];
+    cmd.push(inputPath);
+
+    return cmd;
+  }
+}
