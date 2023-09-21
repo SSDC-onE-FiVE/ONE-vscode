5bdc7375a1538aa8fe264880d0109081f410d492
hohee-hee <khappy517@gmail.com>
2023-09-21 16:40:12 +0900

    [EdgeTPU] Cfg Editor Test init
    - EdgeTpuCfgData.test.ts init
    - Set sample test text
    
    ONE-vscode-DCO-1.0-Signed-off-by: hohee-hee <khappy517@gmail.com>

---
+++ src/Tests/CfgEditor/EdgeTpuCfgData.test.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { assert } from "chai";
+import * as ini from "ini";
+
+import { EdgeTpuCfgData } from "../../CfgEditor/EdgeTpuCfgData";
+
+// NOTE
+// sampleEdgeTpuCfgText and sampleEdgeTpuCfgText1 are the same.
+// But others are different.
+const sampleEdgeTpuCfgText = `
+[edgetpu-compiler]
+edgetpu-compile=True
+edgetpu-profile=False
+
+[edgetpu-compile]
+input_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model.tflite
+output_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model_edgetpu.tflite
+`;
+
+const sampleEdgeTpuCfgText1 = `
+[edgetpu-compile]
+input_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model.tflite
+output_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model_edgetpu.tflite
+
+[edgetpu-compiler]
+edgetpu-compile=True
+edgetpu-profile=False
+`;
+
+const sampleEdgeTpuCfgText2 = `
+[edgetpu-compiler]
+edgetpu-compile=True
+edgetpu-profile=False
+
+[edgetpu-compile]
+input_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model.tflite
+output_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model_edgetpu.tflite
+intermediate_tensors=opr1
+show_operations=True
+`;
+
+const sampleEdgeTpuCfgText3 = `
+[edgetpu-compiler]
+edgetpu-compile=True
+edgetpu-profile=False
+
+[edgetpu-compile]
+input_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model.tflite
+output_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model_edgetpu.tflite
+show_operations=True
+search_delegate=True
+delegate_search_step=1
+`;
+
+const duplicateEdgeTpuCfgTest=`
+[edgetpu-compiler]
+edgetpu-compile=True
+edgetpu-profile=False
+
+[edgetpu-compile]
+input_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model.tflite
+output_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model_edgetpu.tflite
+intermediate_tensors=opr1
+show_operations=True
+search_delegate=True
+delegate_search_step=1
+`;
+
+const duplicateEdgeTpuCfgTest2=`
+[edgetpu-compiler]
+edgetpu-compile=True
+edgetpu-profile=False
+
+[edgetpu-compile]
+input_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model.tflite
+output_path=/home/sohee/ONE-vscode/res/modelDir/truediv/model_edgetpu.tflite
+intermediate_tensors=opr1
+show_operations=True
+search_delegate=True
+`;
+
+suite("EdgetpuCfgEditor", function () {
+  suite("EdgetpuCfgData", function () {
+    suite("#constructor()", function () {
+      test("is constructed", function () {
+        const data = new EdgeTpuCfgData();
+        assert.instanceOf(data, EdgeTpuCfgData);
+      });
+    });
+  });
+}); 
